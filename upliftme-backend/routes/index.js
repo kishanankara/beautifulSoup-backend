40d74fda86ddfe67ca6ba82697aa6ebce20e875f
Aditya Kishan <akishan@buffalo.edu>
2018-04-05 10:32:21 -0400

    initial for beta

--- upliftme-backend/routes/index.js
+++ upliftme-backend/routes/index.js
@@ -1,5 +1,11 @@
 var express = require('express');
 var router = express.Router();
+var mongo = require('mongodb');
+var assert= require('assert');
+
+var url = 'mongodb://localhost:27017';
+
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
